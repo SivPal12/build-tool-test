dc43b73cdfac9d894a69c96609cf4d74f3e65416
Pal Sivertsen <palmarius@hotmail.com>
2014-07-13 22:01:11 +0200

    Added basic test

---
+++ src/main/java/Sample.java
@@ -0,0 +1,5 @@
+public class Sample {
+	public static  String hello() {
+		return "Hello";
+	}
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void test() {
+		assertEquals("Hello", Sample.hello());
+	}
+}
+
